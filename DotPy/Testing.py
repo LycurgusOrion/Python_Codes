70dcbcea57085e89dea4934c1c45ed5ef811b3c7
LycurgusOrion <gamer.forever.untill.death@gmail.com>
2019-01-08 08:48:36 +0530

    Updated Codes

--- DotPy/Testing.py
+++ DotPy/Testing.py
@@ -5,8 +5,18 @@ time.sleep(2)
 
 pg.PAUSE = 0.1
 
-pg.hotkey("alt", "tab")
-pg.hotkey("ctrl", "t")
-pg.hotkey("ctrl", "l")
-pg.typewrite("Python is fun!")
+# Display tree of current directory
+pg.hotkey("ctrl", "alt", "t")
+time.sleep(4)
+pg.typewrite("tree")
 pg.press("enter")
+
+time.sleep(6)
+
+# Select the tree and copy it
+pg.keyDown("ctrl")
+pg.keyDown("shift")
+pg.keyDown("up")
+pg.keyUp("up")
+pg.keyUp("shift")
+pg.keyUp("ctrl")
